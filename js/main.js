2789e5e577f38c957412f6455801a0aeda8e7b87
Deki97 <dejan97@libero.it>
2021-11-15 18:38:06 +0100

    done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // APERTURA (CLICK) DEL BURGER MENU
-const apriBurgerMenu = document.querySelector('.header-right i');
+const apriBurgerMenu = document.querySelector('.header-right > a');
 apriBurgerMenu.addEventListener('click', 
     function() {
         const burgerMenu = document.querySelector('.hamburger-menu');
@@ -8,7 +8,7 @@ apriBurgerMenu.addEventListener('click',
 )
 
 // CHIUSURA (CLICK) DEL BURGER MENU, TRAMITE LA X
-const chiudiBurgerMenu = document.querySelector('.hamburger-menu i');
+const chiudiBurgerMenu = document.querySelector('.hamburger-menu > a');
 chiudiBurgerMenu.addEventListener('click', 
     function() {
         const burgerMenu = document.querySelector('.hamburger-menu');
