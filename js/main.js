ad53eed245cfafbcdd785cc75701023718a75635
Deki97 <dejan97@libero.it>
2021-11-15 16:50:47 +0100

    done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
+// APERTURA (CLICK) DEL BURGER MENU
 const apriBurgerMenu = document.querySelector('.header-right i');
-// console.log(apriBurgerMenu);
-
 apriBurgerMenu.addEventListener('click', 
     function() {
         const burgerMenu = document.querySelector('.hamburger-menu');
-        // console.log(burgerMenu);
         burgerMenu.style.display = "block";
+    }
+)
+
+// CHIUSURA (CLICK) DEL BURGER MENU, TRAMITE LA X
+const chiudiBurgerMenu = document.querySelector('.hamburger-menu i');
+chiudiBurgerMenu.addEventListener('click', 
+    function() {
+        const burgerMenu = document.querySelector('.hamburger-menu');
+        burgerMenu.style.display = "none";
     }
 )
