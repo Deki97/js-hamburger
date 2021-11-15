8c04118582df7e91e1fdd8a2857cb7833b261c7c
Deki97 <dejan97@libero.it>
2021-11-15 16:05:03 +0100

    test burger

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const apriBurgerMenu = document.querySelector('.header-right i');
+// console.log(apriBurgerMenu);
+
+apriBurgerMenu.addEventListener('click', 
+    function() {
+        alert('Ciao hai aperto il burger');
+    }
+)
